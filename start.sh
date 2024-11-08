4468ca27d8144d55120e828b6f18ef23aba11237
Josh Herrera <bluedude227@gmail.com>
2024-11-08 05:50:58 +0000

    Updates

--- start.sh
+++ start.sh
@@ -12,6 +12,43 @@ if [[ -z "${EVERGREEN_USER:-}" || ! "$EVERGREEN_USER" =~ ^[a-zA-Z0-9_]+$ ]]; the
     exit 1
 fi
 
+# Validate EVERGREEN_PASS - allow alphanumeric and typical special characters, adjust as needed
+if [[ -z "${EVERGREEN_PASS:-}" || ! "$EVERGREEN_PASS" =~ ^[a-zA-Z0-9@#%^+=_-]+$ ]]; then
+    echo "Error: EVERGREEN_PASS environment variable is not set, empty, or contains illegal characters."
+    exit 1
+fi
+
+# Install packages
+apk update
+if [ ! -f "/usr/bin/wget" ]; then
+    apk add wget --no-cache
+fi
+if [ ! -f "/usr/bin/ssh-keyscan" ]; then
+    apk add openssh --no-cache
+fi
+
+# Set up SSH server
+if [ ! -f "/etc/ssh/ssh_host_ed25119_key" ]; then
+    echo "Setting up SSH server..."
+    ssh-keygen -A
+fi
+
+# Start SSH server
+if [ ! -f "/var/run/sshd.pid" ]; then
+    echo "Starting SSH server..."
+    /usr/sbin/sshd
+fi
+
+
+# Set up user from env vars if they don't exist
+if ! id "${EVERGREEN_USER}" >/dev/null 2>&1; then
+    echo "Creating sudo user..."
+    adduser -D "${EVERGREEN_USER}"
+    echo "${EVERGREEN_USER}:${EVERGREEN_PASS}" | chpasswd
+    adduser "${EVERGREEN_USER}" wheel
+    echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+fi
+
 # Fetch SSH keys from GitHub for user in GITHUB_USERNAME
 AUTHORIZED_KEYS_PATH="/home/${EVERGREEN_USER}/.ssh/authorized_keys"
 if [ ! -f "${AUTHORIZED_KEYS_PATH}" ]; then
